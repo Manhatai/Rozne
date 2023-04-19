42859d7de243fe66d8e011839b946c45cd9edd7d
Manhatai <131269530+Manhatai@users.noreply.github.com>
2023-04-19 18:59:15 +0200

    Delete zad2.sh

--- zad2.sh
+++
@@ -1,6 +0,0 @@
-git config --global user.email "matixxx.mp4@gmail.com"
-git config --global user.name "Manhatai"
-git init
-git add zad1.sh
-git commit -m"opis commitu"
-git push git push --set-upstream https://github.com/Manhatai/GIT.git main
